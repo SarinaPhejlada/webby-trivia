0f706088916b1e0bb5823d9eee7de67f8046a8f4
Sarina Phejlada <sarina1116@gmail.com>
2017-09-29 14:20:06 -0400

    Add files via upload

---
+++ categories.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>Webby Trivia - Categories</title>
+        <style>
+            html, body{
+                margin: 0;
+                padding: 0;
+                font-family: Arial;
+            }
+            body{
+                background-color: #37E990;
+                color: #000;
+
+            }
+            .title{
+                width: 100%;
+                height: 200px;
+                font-size: 83px;
+                text-align: center;
+            }
+            .categories{
+                position: fixed;
+                text-align: center;
+                margin: 0 auto;
+                width: 100%
+            }
+        </style>
+    </head>
+    <body>
+        <div class = "title">
+            Select a Category:
+        </div>
+        <div class = "categories">
+            <a href = "html.php">Category 1: HTML</a>
+            <a href = "css.php">Category 2: CSS</a>
+            <a href = "php.php">Category 3: PHP</a>
+            <a href = "plc.php">Category 4: PLC</a>
+        </div>
+    </body>
+</html>
